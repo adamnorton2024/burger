d61b094dd89e0059416ef04bb85329d536008a0c
Adam Norton <adam@adamnorton.com>
2019-10-26 16:38:23 -0500

    setup connection to mysql

---
+++ config/connections.js
@@ -0,0 +1,22 @@
+// Set up MySQL Connection
+var mysql = require('mysql');
+
+var connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "C5pkhi6r12!",
+    batabase: "burgers_db"
+});
+
+// Make Connection
+connection.connect(function(err){
+    if(err){
+        console.error("error connecting: " + err.stack);
+        return;
+    }
+    console.log("connected as id " + connection.threadId);
+});
+
+// Export
+module.exports = connection;
