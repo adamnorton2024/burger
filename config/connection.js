b02dfb2099304210eb25ac929d19af832ff10487
Adam Norton <adam@adamnorton.com>
2019-10-28 19:55:30 -0500

    finished styling, and fixed some layout bugs for responsiveness.

--- config/connection.js
+++ config/connection.js
@@ -5,7 +5,7 @@ var connection = mysql.createConnection({
         host: "localhost",
         port: 3306,
         user: "root",
-        password: "C5pkhi6r",
+        password: "C5pkhi6r12!",
         database: "burgers_db"
     });
 
--- db/seeds.sql
+++ db/seeds.sql
@@ -2,4 +2,14 @@
 USE burgers_db;
 -- Insert new rows of data.
 INSERT INTO burgers (burger_name, devoured)
-VALUES ("hamburger", false), ("cheeseburger", false), ("tofu burger", false);
+VALUES ("Texas Chainsaw Massa-Curd Burger", false), 
+("The Final Kraut Down Burger", false), 
+("Foot Feta-ish Burger", false),
+("She's a Super Leek Burger", false),
+("Sweet Chili O' Mine Burger", false),
+("I Fought The Slaw Burger", false),
+("Kales From the Crypt Burger", false),
+("Bohemian Radishy Burger", false),
+("Little Swiss Bunshine Burger", false),
+("Pepper Don't Preach Burger", false),
+("Sweet Home Avocado Burger", false);
